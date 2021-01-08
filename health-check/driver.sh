5bb3f4b28c329fd96d6f2932dac881f00cbc7eda
Maurizio Drocco <maurizio.drocco@ibm.com>
2021-01-08 15:47:20 +0000

    healt-check: support for multi-verifier layouts

--- health-check/driver.sh
+++ health-check/driver.sh
@@ -3,32 +3,40 @@
 set -e
 
 if [ $# -lt 3 ]; then
-    echo "usage: $0 [USER@]VERIFIER_IP BAREMETAL|DOCKER|EXTLOGS LOGTAIL"
+    echo "usage: $0 USER@HOST[,USER@HOST...] BAREMETAL|DOCKER|EXTLOGS LOGTAIL"
     exit
 fi
 
-SSH_VERIFIER=$1; shift
+SSH_LIST=$1; shift
 MODE=$1; shift
 LOGTAIL=$1; shift
 
 echo "> testing SSH connections"
-ssh -q $SSH_VERIFIER exit
-if [ $? -eq 0 ]; then
-    echo "OK: SSH access $SSH_VERIFIER"
-else
-    echo "error: SSH access to $SSH_VERIFIER"
-    exit
-fi
+for h in $(echo $SSH_LIST | sed "s/,/ /g")
+do
+	ssh -q $h exit
+	if [ $? -eq 0 ]; then
+    		echo "OK: SSH access $h"
+	else
+    		echo "error: SSH access to $h"
+    		exit
+	fi
+done
 
 if [[ "$MODE" == "EXTLOGS" && -z ${EXTLOGS_PREFIX+x} ]]; then
     echo "EXTLOGS_PREFIX must be defined for $MODE mode"
     exit
 fi
 
-echo "> uploading and launching the test script on the verifier"
-TMPDIR="$(ssh $SSH_VERIFIER mktemp -d)"
-scp klhc.sh $SSH_VERIFIER:$TMPDIR
-logfile=$(mktemp)
-cmd="ssh $SSH_VERIFIER 'export EXTLOGS_PREFIX=\"$EXTLOGS_PREFIX\"; $TMPDIR/klhc.sh $MODE $LOGTAIL' > $logfile"
-echo $cmd && eval $cmd
-ln -sf $logfile klhc-latest.log
+logdir=$(mktemp -d)
+
+echo "> upload and run the test script"
+for h in $(echo $SSH_LIST | sed "s/,/ /g")
+do
+	TMPDIR="$(ssh $h mktemp -d)"
+	scp klhc.sh $h:$TMPDIR
+	logfile=${logdir}/$(echo $h | cut -f 2 -d '@')
+	cmd="ssh $h 'export EXTLOGS_PREFIX=\"$EXTLOGS_PREFIX\"; $TMPDIR/klhc.sh $MODE $LOGTAIL' > $logfile"
+	echo $cmd && eval $cmd
+done
+ln -sfn $logdir klhc-logs-latest
