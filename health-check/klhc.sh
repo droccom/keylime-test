a8594e41faed5c712c41920935eaf086dcd729c5
Maurizio Drocco <maurizio.drocco@ibm.com>
2021-03-16 21:45:01 +0000

    csv output + refinements

--- health-check/klhc.sh
+++ health-check/klhc.sh
@@ -5,7 +5,8 @@ VERBOSE=1
 set -e
 
 EGREP_UUID="egrep -o [A-Z0-9]+-[A-Z0-9]+-[A-Z0-9]+-[A-Z0-9]+-[A-Z0-9#]+"
-EGREP_REGISTERED="egrep 'adding agent'"
+EGREP_REGISTERED="egrep 'activated'"
+EGREP_ADDED_TO_VERIFIER="egrep 'adding agent'"
 EGREP_IMA="egrep 'Checking IMA'"
 EGREP_FAILED="egrep 'failed'"
 
@@ -17,26 +18,41 @@ fi
 MODE=$1; shift
 LOGTAIL=$1; shift
 
-if [ $MODE == "DOCKER" ]; then
-    CMD_PREFIX="docker logs keylime_verifier | $EGREP_REGISTERED"
-elif [ $MODE == "BAREMETAL" ]; then
-    CMD_PREFIX="journalctl --no-pager -u keylime_verifier.service | $EGREP_REGISTERED"
-elif [ $MODE == "EXTLOGS" ]; then
+# registered
+if [ $MODE == "EXTLOGS" ]; then
     if [[ -z ${EXTLOGS_PREFIX+x} ]]; then
         echo "EXTLOGS_PREFIX must be defined for $MODE mode"
         exit
     fi
-    CMD_PREFIX="sudo $EGREP_REGISTERED ${EXTLOGS_PREFIX}/keylime_verifier-*.log"
+    CMD_PREFIX="sudo $EGREP_REGISTERED ${EXTLOGS_PREFIX}/keylime_registrar-*.log"
+fi
+CMD_REGISTERED_AGENTS="$CMD_PREFIX | $EGREP_UUID"
+
+cmd="$CMD_REGISTERED_AGENTS | sort | uniq"
+if [ "$VERBOSE" == 1 ]; then echo $cmd; fi
+for uid in $(eval $cmd)
+do
+    echo "REGISTERED,$uid"
+done
+
+# added to verifier
+if [ $MODE == "DOCKER" ]; then
+    CMD_PREFIX="docker logs keylime_verifier | $EGREP_ADDED_TO_VERIFIER"
+elif [ $MODE == "BAREMETAL" ]; then
+    CMD_PREFIX="journalctl --no-pager -u keylime_verifier.service | $EGREP_ADDED_TO_VERIFIER"
+elif [ $MODE == "EXTLOGS" ]; then
+    CMD_PREFIX="sudo $EGREP_ADDED_TO_VERIFIER ${EXTLOGS_PREFIX}/keylime_verifier-*.log"
 fi
 CMD_ADDED_AGENTS="$CMD_PREFIX | $EGREP_UUID"
 
-echo "registered agents:"
-cmd="$CMD_ADDED_AGENTS | sort | uniq | wc -l"
+cmd="$CMD_ADDED_AGENTS | sort | uniq"
 if [ "$VERBOSE" == 1 ]; then echo $cmd; fi
-eval $cmd
+for uid in $(eval $cmd)
+do
+    echo "ADDED_TO_VERFIIER,$uid"
+done
 
-echo
-echo "healthy agents over latest $LOGTAIL per-verifier IMA logs:"
+# verified
 if [ $MODE == "DOCKER" ]; then
     CMD_PREFIX="docker logs keylime_verifier | $EGREP_IMA | tail -n $LOGTAIL"
 elif [ $MODE == "BAREMETAL" ]; then
@@ -44,29 +60,33 @@ elif [ $MODE == "BAREMETAL" ]; then
 elif [ $MODE == "EXTLOGS" ]; then
     CMD_PREFIX="sudo tail -n $LOGTAIL ${EXTLOGS_PREFIX}/keylime_verifier-*.log | $EGREP_IMA"
 fi
-cmd="$CMD_PREFIX | $EGREP_UUID | sort | uniq | wc -l"
+cmd="$CMD_PREFIX | $EGREP_UUID | sort | uniq"
 if [[ "$VERBOSE" == 1 ]]; then echo $cmd; fi
-eval $cmd
+for uid in $(eval $cmd)
+do
+    echo "VERIFIED,$uid"
+done
 
-echo
-echo "failed agents:"
+# failed
 if [ $MODE == "DOCKER" ]; then
     for a in $(docker logs keylime_verifier | $EGREP_FAILED | $EGREP_UUID); do
-        echo "failing agent: $a"
-        docker logs keylime_verifier | grep $a | tail -n 2
+        echo "FAIL,$a"
+	docker logs keylime_verifier | grep $a | tail -n 2
     done
 elif [ $MODE == "BAREMETAL" ]; then
     for a in $(journalctl --no-pager -u keylime_verifier.service | $EGREP_FAILED | $EGREP_UUID); do
-        echo "failing agent: $a"
+        echo "FAIL,$a"
     	journalctl --no-pager -u keylime_verifier.service | grep $a | tail -n 2
     done
 elif [ $MODE == "EXTLOGS" ]; then
     for a in $(sudo $EGREP_FAILED ${EXTLOGS_PREFIX}/keylime_verifier-*.log | $EGREP_UUID); do
-        echo "failing agent: $a"
+        echo "FAIL,$a"
     	sudo grep $a ${EXTLOGS_PREFIX}/keylime_verifier-*.log | tail -n 2
     done
 fi
 
+# extra verbose stuff
+if [[ "$VERBOSE" == 1 ]]; then
 echo
 echo "latest log entry:"
 if [ $MODE == "DOCKER" ]; then
@@ -78,6 +98,7 @@ elif [ $MODE == "EXTLOGS" ]; then
 fi
 if [ "$VERBOSE" == 1 ]; then echo $cmd; fi
 eval $cmd
+fi
 
 if [[ $MODE == "EXTLOGS" && "$VERBOSE" == 1 ]]; then
     ls -lt ${EXTLOGS_PREFIX}/keylime_verifier-*.log  
